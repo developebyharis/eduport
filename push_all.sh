5b8fbd854be98c3062a6d7df9b248fe7a6fba5b4
Haris <developewithharis@gmail.com>
2023-07-10 23:54:05 +0500

    update all files

--- push_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Array of branches to push
-branches=("main" "production" )
-
-# Loop through each branch and push to origin
-for branch in "${branches[@]}"; do
-  git push origin "$branch"
-done
